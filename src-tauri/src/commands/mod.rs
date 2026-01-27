7dea10acd4adddd8bdabe9510ddc2050bb8766f5
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2026-01-27 18:59:55 +0530

    bug: migrate to postgresql

--- src-tauri/src/commands/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod file_commands;
-pub mod notes_commands;
-pub mod search_commands;
-pub mod tag_commands;
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,3 @@
-pub mod commands;
-pub mod db;
-
-use crate::db::AppState;
-use std::sync::Mutex;
-use tauri::Manager;
 
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -13,30 +7,8 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .setup(|app| {
-            let conn = db::init_db();
-            app.manage(AppState {
-                db: Mutex::new(conn),
-            });
-            Ok(())
-        })
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![
-            greet,
-            commands::notes_commands::create_note,
-            commands::notes_commands::get_all_notes,
-            commands::notes_commands::update_note,
-            commands::notes_commands::delete_note,
-            commands::tag_commands::create_tag,
-            commands::tag_commands::get_all_tags,
-            commands::tag_commands::delete_tag,
-            commands::tag_commands::add_tag_to_note,
-            commands::tag_commands::remove_tag_from_note,
-            commands::tag_commands::get_tags_for_note,
-            commands::search_commands::search_notes,
-            commands::file_commands::export_note,
-            commands::file_commands::import_note_from_file
-        ])
+        // Here you can add plugins, windows, and other Tauri configurations
+        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
